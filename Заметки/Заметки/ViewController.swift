dbc3f61749162628b8c716bf6bea4197ff25e44f
Федор Гладков <fedor.9619@gmail.com>
2024-02-13 12:29:52 +0700

    initial commit

---
+++ Заметки/Заметки/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Заметки
+//
+//  Created by Федор Гладков on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
